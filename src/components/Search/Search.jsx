036bf9837f048b8cd51aff7e3e6cf18f5fda905f
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-12-13 16:13:48 +0300

    Добавить фокус на инпут при его очистке

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { SearchContext } from '../../App';
 
 function Search() {
     const { searchValue, setSearchValue } = useContext(SearchContext);
+    const inputRef = useRef();
+
+    const onClickClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+    };
+    
     return (
         <form action="" className="nav__search search-form">
             <div className="search-form__icon">
                 <img src="./../img/icons/search.svg" alt="search" />
             </div>
             <input
+                ref={inputRef}
                 value={searchValue}
                 onChange={(e) => setSearchValue(e.target.value)}
                 type="text"
@@ -17,7 +25,7 @@ function Search() {
             />
             {searchValue && (
                 <div
-                    onClick={() => setSearchValue('')}
+                    onClick={onClickClear}
                     className="search-form__clear-btn"
                 >
                     <img src="./../img/icons/clear.svg" alt="clear" />
